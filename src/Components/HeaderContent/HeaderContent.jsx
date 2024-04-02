ad017e4f61042c44b8899acf41151d0283ab1c1b
herathua <110981456+herathua@users.noreply.github.com>
2024-04-02 15:54:34 +0530

    headder 3

--- src/Components/HeaderContent/HeaderContent.jsx
+++ src/Components/HeaderContent/HeaderContent.jsx
@@ -5,20 +5,20 @@ import lablogo from '../../assets/lab logo.jpeg';
 
 function HeaderContent() {
   return (
-<div className="flex justify-between items-center px-4 py-2 bg-blue-200">
-      <div className='p-2 bg-blue-400'>
-      <div className=" flex items-center space-x-2">
-        <div className="p-2 bg-yellow-300 rounded-full">
-          <img src={logo} alt="HealthHive Logo" className="w-8 h-8" />
+    <div className="flex items-center justify-between px-4 py-2 bg-blue-200">
+      <div className="to-blue-700">
+        <div className="flex items-center space-x-2 ">
+          <div className="p-2 bg-yellow-300 rounded-full">
+            <img src={logo} alt="HealthHive Logo" className="w-8 h-8 " />
+          </div>
+          <h1 className="text-xl font-bold text-blue-800">HealthHive</h1>
         </div>
-        <h1 className="text-xl font-bold text-blue-800">HealthHive</h1>
-      </div>
       </div>
       <div className="flex items-center space-x-2">
         <div className="p-2 bg-white rounded-full">
           <img src={lablogo} alt="ASIRI Logo" className="w-10 h-10" />
         </div>
-        <div className="bg-white rounded py-1 px-3">
+        <div className="px-3 py-2.5 bg-white rounded-full">
           <h2 className="text-lg font-semibold text-blue-800">ASIRI - CENTRAL HOSPITAL</h2>
         </div>
       </div>
